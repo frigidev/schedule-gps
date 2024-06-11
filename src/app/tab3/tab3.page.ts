d58a15dd3943b1bb8e3226542c00cdc9228803cc
Orestes Lucas <oresteslucas@gmail.com>
2024-06-11 12:37:37 -0300

    Update tab3.page.ts

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -7,7 +7,6 @@ import { GoogleMap, Marker } from '@capacitor/google-maps';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -40,14 +39,13 @@ export class Tab3Page {
   constructor(
     private readonly toast: ToastController,
     private readonly zone: NgZone,
-    private navCtrl: NavController,
-    private routes: Router
+    private navCtrl: NavController
   ) {}
 
   ionViewDidEnter(): void {
     this.requestPermissions();
     this.getCurrentInitialPosition().then(() => {
-      this.createMap();
+      this.createNewMap();
     });
   }
 
@@ -71,12 +69,29 @@ export class Tab3Page {
     await this.addMarkers();
   }
 
+  async createNewMap(){
+    this.map = await GoogleMap.create({
+      id: 'map-gps',
+      apiKey: environment.mapsKey!,
+      element: this.mapRef.nativeElement,
+      forceCreate: true,
+      config: {
+        center: {
+          lat: this.latitude!,
+          lng: this.longitude!,
+        },
+        zoom: 8,
+      },
+    });
+    await this.addMarkers();
+  }
+
   /* If it is the first time loading this tab, the map will not load correctly based on the user position.
     This function below navigate to an auxiliary route and guarantee that the map will be loaded correctly,
     otherwhise, an error message is displayed*/
   async reloadPage() {
     this.firstMapLoad = false;
-    await this.routes.navigateByUrl('/tabs/aux-route', {skipLocationChange: true}).then(() => {
+    await this.navCtrl.navigateRoot('/tabs/aux-route', {skipLocationChange: true}).then(() => {
       this.navCtrl.navigateForward('/tabs/tab3', {replaceUrl: true});
     }).catch(async err => {
         const message = await this.toast.create({
@@ -135,9 +150,11 @@ export class Tab3Page {
     const currentLocation = await Geolocation.getCurrentPosition(this.options);
 
     this.latitude = currentLocation?.coords?.latitude;
-    this.longitude = currentLocation?.coords?.longitude;
-    
-    await this.reloadPage();
+    this.longitude = currentLocation?.coords?.longitude;  
+
+    this.reloadPage().then(() => {
+      this.createMap();
+    });
   }
 
   watchPosition() {
